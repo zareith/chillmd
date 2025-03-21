f38b73f4dff8e8fea9c86d1226727960a0d04b66
Zareith <zareith@tuta.com>
2025-03-21 12:44:43 +0530

    Updates

--- assets/readonly-tcxWvTqP-CwJd1o0p.js
+++
@@ -1,1 +0,0 @@
-import{f as o}from"./index-DmRNHBad.js";const t=()=>o.readOnly.of(!0);export{t as readonly};
---
+++ assets/readonly-tcxWvTqP-DOA8u1-3.js
@@ -0,0 +1,1 @@
+import{f as o}from"./index-8XUlHtYq.js";const t=()=>o.readOnly.of(!0);export{t as readonly};
